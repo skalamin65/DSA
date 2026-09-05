356d4fbe6a90c44630faf8fe2d98f5b19b8f834e
Sk Alamin Hossain <skalamin3030@gmail.com>
2026-09-05 22:13:02 +0530

    Time: 8 ms (28.53%) | Memory: 165.9 MB (55.78%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // Sk Alamin Hossain
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        int minEven = INT_MAX;
+        bool hasOdd = false, hasEven = false;
+        
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                hasOdd = true;
+                minOdd = min(minOdd, x);
+            } else {
+                hasEven = true;
+                minEven = min(minEven, x);
+            }
+        }
+        
+        if (!hasOdd || !hasEven) {
+            return true;
+        }
+        
+        return minOdd < minEven;
+    }
+};
