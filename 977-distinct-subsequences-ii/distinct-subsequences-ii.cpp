bb3cf2924d1cfe6d4a3b1312d56d28aa2a97bfbe
Sk Alamin Hossain <skalamin3030@gmail.com>
2026-09-07 11:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (22.93%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -5,12 +5,9 @@ public:
         const int MOD = 1e9 + 7;
         int n = s.size();
         
-        // dp[i] = number of distinct subsequences (including empty) 
-        // using first i characters
         vector<long long> dp(n + 1, 0);
         dp[0] = 1;
         
-        // last[c] = last index (1-indexed) where character c appeared
         vector<int> last(26, -1);
         
         for (int i = 1; i <= n; i++) {
@@ -18,15 +15,12 @@ public:
             dp[i] = (2 * dp[i - 1]) % MOD;
             
             if (last[c - 'a'] != -1) {
-                // subtract subsequences counted twice 
-                // (those already ending in c from previous occurrence)
                 dp[i] = (dp[i] - dp[last[c - 'a'] - 1] + MOD) % MOD;
             }
             
             last[c - 'a'] = i;
         }
         
-        // subtract 1 for the empty subsequence
         return (int)((dp[n] - 1 + MOD) % MOD);
     }
 };
