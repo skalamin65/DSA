29c8cc6bba73041a1a9d6ec6cebb02204fecb1a7
Sk Alamin Hossain <skalamin3030@gmail.com>
2026-09-19 21:36:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.85%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // Sk Alamin Hossain
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int closestX = max(x1, min(xCenter, x2));
+        int closestY = max(y1, min(yCenter, y2));
+        
+        long long dx = xCenter - closestX;
+        long long dy = yCenter - closestY;
+        
+        return dx * dx + dy * dy <= (long long)radius * radius;
+    }
+};
